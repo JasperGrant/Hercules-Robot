d355d7a98e2288f061bc773c90da549bede452f9
Andrew Doucet <andrew.doucet@dal.ca>
2022-06-16 18:41:28 -0300

    made changes to code

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -13,17 +13,17 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "simple_navigation_goals");
 
 	//tell the action client that we want to spin a thread by default
-	MoveBaseClient ac("odom", true);
+	MoveBaseClient ac("move_base", true);
 
 	//wait for the action server to come up
 	while(!ac.waitForServer(ros::Duration(5.0)))
 	{
 		ROS_INFO("Waiting for the move_base action server to come up");
 	}
-
+	
 	move_base_msgs::MoveBaseGoal goal;
 
-	for(int i=0;i<4;i++)
+	for(int i=0;i<1;i++)
 	{
 	//we'll send a goal to the robot to move 1 meter forward
 	goal.target_pose.header.frame_id = "base_link";
