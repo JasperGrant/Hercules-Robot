3b164458b0e66f8f7f26e2bcb0cb8648b85d8101
JasperGrant <72110751+JasperGrant@users.noreply.github.com>
2022-07-07 18:31:35 -0300

    Added forward case

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -3,26 +3,31 @@
 #include <actionlib/client/simple_action_client.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 #define PI 3.1415926
 
-#define MAXLEN 6
+
+#define MAXSTRINGLEN 6
+#define MAPSIZE 7
+#define NUMBEROFMAPS 3
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-void gohere(COORD * coord, MoveBaseClient &ac)
+void gohere(int x, int y, MoveBaseClient &ac)
 {
 	move_base_msgs::MoveBaseGoal goal;
 	goal.target_pose.header.frame_id = "map";
 	goal.target_pose.header.stamp = ros::Time::now();
 
-	goal.target_pose.pose.position.x = coord->x;
-	goal.target_pose.pose.position.y = coord->y;
+	goal.target_pose.pose.position.x = x;
+	goal.target_pose.pose.position.y = y;
 
 	goal.target_pose.pose.orientation.w = 1.0;
 	ROS_INFO("Sending goal");
 
 	ac.sendGoal(goal);
+    //TODO: Find out way to make sure we are not skipping the new instruction inside this loop
 	ac.waitForResult();
 	
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
@@ -32,86 +37,36 @@ void gohere(COORD * coord, MoveBaseClient &ac)
 }
 //Define map structure
 //Note: mine map is flipped to the right from what you would expect.
-char forwards_map[7][7][MAXLEN] = {" ", " ", " ", " ", " ", " ", " ",
+char map[NUMBEROFMAPS][MAPSIZE][MAPSIZE][MAXSTRINGLEN] = {" ", " ", " ", " ", " ", " ", " ",
                                " ", " ", " ", " ", " ", " ", " ",
                                " ", " ", " ", " ", " ", " ", " ",
                                " ", " ", " ", " ", " ", " ", " ",
                                " ", " ", " ", " ", " ", " ", " ",
                                " ", " ", " ", " ", " ", " ", " ",
                                " ", " ", " ", " ", " ", " ", " ",
+                                      " ", " ", " ", " ", " ", " ", " ",
+                                      " ", " ", " ", " ", " ", " ", " ",
+                                      " ", " ", " ", " ", " ", " ", " ",
+                                      " ", " ", " ", " ", " ", " ", " ",
+                                      " ", " ", " ", " ", " ", " ", " ",
+                                      " ", " ", " ", " ", " ", " ", " ",
+                                      " ", " ", " ", " ", " ", " ", " ",
+                             " ", " ", " ", " ", " ", " ", " ",
+                             " ", " ", " ", " ", " ", " ", " ",
+                             " ", " ", " ", " ", " ", " ", " ",
+                             " ", " ", " ", " ", " ", " ", " ",
+                             " ", " ", " ", " ", " ", " ", " ",
+                             " ", " ", " ", " ", " ", " ", " ",
+                             " ", " ", " ", " ", " ", " ", " ",
 
 };
 
-char backwards_map[7][7][MAXLEN] = {" ", " ", " ", " ", " ", " ", " ",
-                                  " ", " ", " ", " ", " ", " ", " ",
-                                  " ", " ", " ", " ", " ", " ", " ",
-                                  " ", " ", " ", " ", " ", " ", " ",
-                                  " ", " ", " ", " ", " ", " ", " ",
-                                  " ", " ", " ", " ", " ", " ", " ",
-                                  " ", " ", " ", " ", " ", " ", " ",
-
+enum map_choice{
+    forwards, reversing, returning
 };
 
-struct coordinate{
-    int x;
-    int y;
-    struct coordinate *next;
-} typedef COORD;
-
-struct mempool{
-    struct coordinate *head;
-    struct coordinate *tail;
-} typedef MEMPOOL;
-
-COORD *new_coordinate(void){
-    //Allocate memory
-    COORD *new_coord = (COORD *)calloc(1, sizeof(COORD));
-    //Zero components of coord
-    new_coord->x = 0;
-    new_coord->y = 0;
-    //Return new zeroed coordinate
-    return new_coord;
-}
-
-MEMPOOL *new_mempool(void){
-    //Allocate memory
-    MEMPOOL *new_mempool = (MEMPOOL *) calloc(1, sizeof(MEMPOOL));
-    //Make NULL pointers
-    new_mempool->head = NULL;
-    new_mempool->tail = NULL;
-    //Return NULLed linked list
-    return new_mempool;
-
-}
-
-
-COORD *closest(COORD * coord, char c){
-    //Define return result
-    COORD *result = new_coordinate();
-    //Float to tell distance from a mine
-    float min = 20, distance;
-    //For each square
-    for(int X = 0; X<7; X++){
-        for(int Y = 0; Y<7; Y++){
-            //If mine
-            if(mine_map[X][Y] == c){
-                distance = sqrt(((X-coord->x)*(X-coord->x)) + ((Y-coord->y)*(Y-coord->y)))
-                //If closer to then previous min
-                if(distance < min){
-                    //Update result
-                    min = distance;
-                    result->x = X;
-                    result->y = Y;
-                }
-            }
-        }
-    }
-    //Ternary operator, if coordinate has not beeen changed no mine has been found so return NULL
-    return (result->x == 0 || result->y == 0) ? (NULL) : (result);
-
-}
-
-//TODO: Find way to add current position
+//TODO: Subsciber for position
+//TODO: Subscriber for limit switches
 
 int main(int argc, char** argv)
 {
@@ -126,11 +81,46 @@ int main(int argc, char** argv)
 		ROS_INFO("Waiting for the move_base action server to come up");
 	}
 
-    //Define mine count as 0
-    int mine_count = 0;
+    //Integers to represents x and y coordinates
+    int x = 0, y = 0;
+    //Enum to be swapped out to specify different maps
+    enum map_choice maps = forwards;
     //Main loop
     for(;;){
-        for(int i = 0; ;)
+        //Loop to go through string
+        for(int i = 0; map[maps][x][y][i] != '\0';i++){
+            //Switch to convert cardinal direction in string into coordinate change
+            switch(){
+                case 'E':
+                    //Move position one east
+                    x++;
+                    break;
+                case 'W':
+                    //Move position one west
+                    x--;
+                    break;
+                case 'N':
+                    //Move position one north
+                    y++;
+                    break;
+                case 'S':
+                    //Move position one south
+                    y--;
+                    break;
+                case 'R':
+                    //Change map to return map
+                    maps = returning;
+                    //Go to next iteration of loop without going anywhere
+                    continue;
+                default:
+                    ROS_INFO("Unrecognized character in instruction string\n")
+                    break;
+
+            }
+            //Go to decided on location
+            gohere(x, y , ac);
+        }
+
         //Get location and do instructions from forward squares
         //If callback is activated:
             //Switch to backward squares
