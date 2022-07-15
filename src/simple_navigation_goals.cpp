e25eb690b84256faac958ae87de9269c3c953469
Spencer Dugas <email@dal.ca>
2022-07-14 21:47:06 -0300

    Modified ofsets

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -61,8 +61,8 @@ char map[NUMBEROFMAPS][MAPSIZE][MAPSIZE][MAXSTRINGLEN] = {
 // Offset MapsNNNWWH
 // X Offsets followed by Y offsets
 float offsets[NUMBEROFOFFSETS][MAPSIZE][MAPSIZE] = {
-        0.0, 0.0, 0.15, 0.15, 0.15, 0.1, 0.05,
-        0.0, 0.0, 0.05, 0.0, 0.0, 0.0, -0.05,
+        0.0, 0.0, 0.10, 0.15, 0.10, 0.1, 0.1,
+        0.0, 0.0, 0.10, 0.0, 0.0, 0.0, -0.05,
         0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.1,
         0.0, 0.0, 0.0, -0.5, 0.0, 0.0, 0.0,
         0.0, 0.0, 0.5, 0.1, 0.2, 0.1, 0.0,
